cbb678c366b18b50b7bc27e3853b9867f4ccbb85
stanley zheng <dev.stanzheng@gmail.com>
2014-10-04 08:05:00 -0700

    add photo image

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -45,4 +45,33 @@ angular.module('todoApp.controllers',[]).controller('TodoListController',['$scop
     $scope.create=function(){
       $state.go('todos');
     };
+    $scope.takePic = function() {
+     var options =   {
+         quality: 50,
+         //destinationType: Camera.DestinationType.FILE_URI,
+         sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+         encodingType: 0     // 0=JPG 1=PNG
+     };
+     navigator.camera.getPicture(onSuccess,onFail,options);
+    };
+    var onSuccess = function(FILE_URI) {
+     console.log(FILE_URI);
+     $scope.picData = FILE_URI;
+     $scope.$apply();
+    };
+    var onFail = function(e) {
+     console.log("On fail " + e);
+    };
+    $scope.send = function() {
+     var myImg = $scope.picData;
+     var options = new FileUploadOptions();
+     options.fileKey="post";
+     options.chunkedMode = false;
+     var params = {};
+     params.user_token = localStorage.getItem('auth_token');
+     params.user_email = localStorage.getItem('email');
+     options.params = params;
+     var ft = new FileTransfer();
+     ft.upload(myImg, encodeURI("https://example.com/posts/"), onUploadSuccess, onUploadFail, options);
+   };
 }]);
